e0d273a1d6589169926c4e75c3e80e3555f1cb10
Catt Small <cattsmall@gmail.com>
2018-02-13 21:14:22 -0500

    Removed HTML list items that were moved to JSON

--- scripts.js
+++ scripts.js
@@ -24,8 +24,8 @@ loadJSON(function(response) {
 
     // Sort by name
     personData.sort(function(a, b) {
-        if(a.name < b.name) return -1;
-        if(a.name > b.name) return 1;
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
         return 0
     });
 
